2ba11e7769049e90913129ccf560174f7b8d9abd
ashvitha2005 <rashvitha@Rs-MacBook-Air.local>
2026-07-08 18:40:57 +0530

    Add birthday reveal overlay and music

--- birthday.js
+++ birthday.js
@@ -1,4 +1,16 @@
-const birthdaySound = new Audio ('assets/happy-birthday.mp3');
-
+const clickSound = new Audio('assets/click.mp3');
+const birthdaySound = new Audio("assets/happy-birthday.mp3");
 birthdaySound.loop = true;
-birthdaySound.play();
+
+const overlay = document.querySelector(".overlay");
+const revealButton = document.querySelector(".revealbutton");
+const birthdayContent = document.querySelector('.birthday-content');
+
+revealButton.addEventListener('click', function () {
+    clickSound.currentTime = 0;
+    clickSound.play();
+    
+    birthdaySound.play();
+    overlay.style.display = 'none';
+    birthdayContent.style.display = 'flex';
+});
